afc11b12e5a0c4a6f19f0a98d110a28c5802b64a
suxitman <48613563+suxitman@users.noreply.github.com>
2019-03-16 13:42:00 +0700

    add this in

---
+++ demo.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)demo.java
+ *
+ * demo application
+ *
+ * @author 
+ * @version 1.00 2019/3/16
+ */
+ 
+public class demo {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
